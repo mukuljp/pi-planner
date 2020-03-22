5862b436e52c2a52c08cb4a28768307b65e68373
Mukul Jayaprakash <mukuljp@gmail.com>
2020-03-23 01:24:04 +0530

    Updates

---
+++ precache-manifest.1443932ee40799f7f582240cd60928f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "127a9b81f11c5eacf1b0dd5f000e5e40",
+    "url": "/pi-planner/index.html"
+  },
+  {
+    "revision": "cf24b9f16d098ba124b2",
+    "url": "/pi-planner/static/css/2.2bd37e2f.chunk.css"
+  },
+  {
+    "revision": "cf24b9f16d098ba124b2",
+    "url": "/pi-planner/static/js/2.23802730.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/pi-planner/static/js/2.23802730.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06e3c332a462c9ba0db9",
+    "url": "/pi-planner/static/js/main.021a4f8c.chunk.js"
+  },
+  {
+    "revision": "52712f7861cf6a86893e",
+    "url": "/pi-planner/static/js/runtime-main.a7d3cd94.js"
+  }
+]);
